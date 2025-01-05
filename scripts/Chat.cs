d1f86b83e20817df16839715d64867f42c33f6b9
Riley <waskerdu@gmail.com>
2025-01-05 15:14:27 -0800

    text log

---
+++ scripts/Chat.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class Chat : Node
+{
+    TextEdit edit;
+    VBoxContainer container;
+    public override void _Ready()
+    {
+        edit = GetChild<TextEdit>(0);
+        container = GetChild(1).GetChild<VBoxContainer>(0);
+        edit.TextChanged += ()=>{
+            if(edit.Text[edit.Text.Length-1] == '\n'){
+                Submit(edit.Text);
+                edit.Text = "";
+            }
+        };
+    }
+    void Submit(string text){
+        Label label = new()
+        {
+            Text = text
+        };
+        container.AddChild(label);
+    }
+}
